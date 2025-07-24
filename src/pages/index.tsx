d957fd01b0ed70b6fd4820797e705ac285817f40
Yuri Brasil <yurialvesbrasil@gmail.com>
2025-07-24 08:08:21 -0300

    Ajustes de layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,11 @@
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { HeroSection } from "@/components/hero-section";
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-    </div>
+    <>
+      <article>
+        <HeroSection />
+      </article>
+    </>
   );
 }
