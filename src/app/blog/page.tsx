31a93ce1d5913ecfc784453fc5221fc74abed310
Yuri Brasil <yurialvesbrasil@gmail.com>
2025-09-04 15:48:50 -0300

    SEO em blog pagen list

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { BlogList } from '@/templates/blog';
 import { allPosts } from 'contentlayer/generated';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'Dicas e estratégias para impulsionar seu negócio',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'Dicas e estratégias para impulsionar seu negócio',
+    url: 'https://nextjs-fundamentos-do-next.vercel.app/op-image.jpg',
+    siteName: 'Blog',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://nextjs-fundamentos-do-next.vercel.app/op-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Blog',
+      },
+    ],
+  },
+};
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
