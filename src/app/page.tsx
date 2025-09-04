afed965461b10e4da03223e2f57bdd5778e6c20c
Yuri Brasil <yurialvesbrasil@gmail.com>
2025-09-04 15:23:22 -0300

    fix: Criação de metatags

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,28 @@
+import type { Metadata } from 'next';
 import { LandingPage } from '@/templates/landing-page';
+
+
+export const metadata: Metadata = {
+  title: 'Site.Set',
+  description: 'Venda seus produtos como afiliado em um único lugar',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.Set',
+    description: 'Venda seus produtos como afiliado em um único lugar',
+    url: 'https://nextjs-fundamentos-do-next.vercel.app/og-image.jpg',
+    siteName: 'Site.Set',
+    locale: 'pt-BR',
+    type: 'website',
+    images: [
+      {
+        url: 'https://nextjs-fundamentos-do-next.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.Set',
+      },
+    ],
+  },
+};
 
 export default function HomePage() {
   return <LandingPage />;
