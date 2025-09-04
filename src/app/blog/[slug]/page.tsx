db323ae775c90f51cdfe0961e14f82e458ccc0be
Yuri Brasil <yurialvesbrasil@gmail.com>
2025-09-04 15:58:37 -0300

    SEO dos blogs

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,34 @@
 import { PostPage } from '@/templates/blog';
 import { allPosts } from 'contentlayer/generated';
+import { Metadata } from 'next';
 import { notFound } from 'next/navigation';
 
-type BlogPostPage = {
+type BlogPostPageProps = {
   params: Promise<{
     slug: string;
   }>;
 };
+
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = allPosts.find((post) => post.slug === slug);
+
+  if (!post) {
+    return {};
+  }
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+    robots: 'index, follow',
+    openGraph: {
+      images: [post.image ?? 'https://nextjs-fundamentos-do-next.vercel.app/op-image.jpg'],
+    },
+  };
+}
 
 export const revalidate = 60;
 
@@ -20,7 +42,7 @@ export async function generateStaticParams() {
   }));
 }
 
-export default async function BlogPostPage({ params }: BlogPostPage) {
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
   const { slug } = await params;
   const post = allPosts.find((post) => post.slug === slug);
   console.log(slug);
